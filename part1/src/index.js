ce200f019c6d1a0d249b20253bb6454b1c876eda
Vinh Kieu <transformers9510@gmail.com>
2021-02-21 22:46:29 +0700

    1.4

--- part1/src/index.js
+++ part1/src/index.js
@@ -5,34 +5,36 @@ import Content from "./Content";
 import Total from "./Total";
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
 
   return (
     <div>
       <Header course={course} />
       <Content
-        part1={part1.name}
-        exercises1={part1.exercises}
-        part2={part2.name}
-        exercises2={part2.exercises}
-        part3={part3.name}
-        exercises3={part3.exercises}
+        part1={parts[0].name}
+        exercises1={parts[0].exercises}
+        part2={parts[1].name}
+        exercises2={parts[1].exercises}
+        part3={parts[2].name}
+        exercises3={parts[2].exercises}
       />
       <Total
-        exercises1={part1.exercises}
-        exercises2={part2.exercises}
-        exercises3={part3.exercises}
+        exercises1={parts[0].exercises}
+        exercises2={parts[1].exercises}
+        exercises3={parts[2].exercises}
       />
     </div>
   );
